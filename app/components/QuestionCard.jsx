0fe08ba60d553389762bb4593df626b3dc28f4e0
Bishop Zareh <xchicago@gmail.com>
2016-06-28 15:20:40 -0600

    added bootstrap modal window

--- app/components/QuestionCard.jsx
+++ app/components/QuestionCard.jsx
@@ -1,15 +1,29 @@
+var $ = global.$ = global.jQuery = require('jquery');
+
 import React, {PropTypes, Component} from 'react';
 
 class QuestionCard extends Component {
+  componentDidMount () {
+    require('bootstrap');
+    $('.modal').modal({show: true, backdrop: 'static'});
+  }
   render() {
     const questionData = this.props.question;
+    var answerButtons = questionData.answers.map(function(answer){
+      return <button type="button" className="btn btn-default">{answer.text}</button>;
+    });
     return (
-      <div>
-        <h2 className='pageTitle'>{questionData.question}</h2>;
-        <ul className='answers'>
-          <li><button className='btn'>{questionData.answers[0].text}</button></li>
-          <li><button className='btn'>{questionData.answers[1].text}</button></li>
-        </ul>
+      <div className="modal fade" tabIndex="-1" role="dialog">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-body">
+              <h4>{questionData.question}</h4>
+            </div>
+            <div className="modal-footer">
+              {answerButtons}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
